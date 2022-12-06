9683c7dd56572981e74b5c37057f5695dd84fd3c
Heyswor <andreydymov93@gmail.com>
2022-12-07 01:24:20 +0200

    react-hw-03-image-finder part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+// import {} from '';
+import { Searchbar } from './Searchbar/Searchbar';
+
+// url https://pixabay.com/api/?q=cat&page=1&key=30513394-8e383ed067439270a89ebf2b5&image_type=photo&orientation=horizontal&per_page=12
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Searchbar />
     </div>
   );
 };
